3220cb246e4dfbac16f4f5c9f4f93fcc3e87fd6a
Simone Rigoni <simone.rigoni01@gmail.com>
2019-07-06 19:04:24 +0200

    Add C# hello world

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+/* Print 'Hello World!' on standard output
+   csc HelloWorld.cs
+   ./HelloWorld.exe
+*/ 
+
+using System;
+
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
