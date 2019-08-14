d7d6b160a98918628847334b8d079026db820737
Simone Rigoni <simone.rigoni01@gmail.com>
2019-08-14 14:02:13 +0200

    Use single line comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,6 @@
-/* Print 'Hello World!' on standard output
-   g++ -o hello_world.exe hello_world.cpp
-   ./hello_world.exe
- */
+// Print 'Hello World!' on standard output
+// g++ -o hello_world.exe hello_world.cpp
+// ./hello_world.exe
 
 #include <iostream>
 using namespace std;
