4f4e8cd00924138b7a0d352e34a26487ac5d659d
simone <simone.rigoni01@gmail.com>
2025-03-11 15:00:11 +0100

    Add hello_world.js

---
+++ hello_world.js
@@ -0,0 +1,18 @@
+// Node.js Hello World web server
+// node hello_world.js
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+// http://localhost:3000/
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
