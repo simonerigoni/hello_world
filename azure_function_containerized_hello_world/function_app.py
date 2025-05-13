bbcc9ccd4381781e7383085987cb0548a0c1a3d7
simonerigoni <simone.rigoni00@gmail.com>
2025-05-13 17:23:47 +0200

    Add logging

--- azure_function_containerized_hello_world/function_app.py
+++ azure_function_containerized_hello_world/function_app.py
@@ -1,5 +1,6 @@
 import platform
 import azure.functions as func
+import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -7,6 +8,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info(f"From http_trigger started execution.")
     os_name = platform.system()
     architecture = platform.architecture()
     return func.HttpResponse(f"Hello World! from {os_name} operating system on {architecture[0]} {architecture[1]} architecture", status_code = 200)
