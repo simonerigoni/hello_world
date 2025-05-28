82b38d368c74cd538002bf9ef74d36739a656973
simonerigoni <simone.rigoni00@gmail.com>
2025-05-28 15:56:06 +0200

    Update client.py

--- docker_client_server_hello_world/client/client.py
+++ docker_client_server_hello_world/client/client.py
@@ -6,11 +6,11 @@ import time
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
 
-# Get server host from environment variable, default to 'server' for Docker
-SERVER_HOST = os.getenv('SERVER_HOST', 'server')
+# Get server host from environment variable
+SERVER_HOST = os.getenv('SERVER_HOST')
 SERVER_URL = f'http://{SERVER_HOST}:5000/hello'
 
 
